0a0b3e8227164863d8a6662358e3775d38f41278
Yuldoshboy <sobirov.yu@gmail.com>
2024-11-22 00:22:15 +0900

    CRUD operatsiyalar va Task B

--- train.js
+++ train.js
@@ -1,21 +1,17 @@
-// TASK-A
-/*Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi.*/
+// B-TASK: 
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
 function task_b(string) {
     let m = 0;
     const a = string.split("");
-    a.forEach(raqamlar => {
-        if(Number(raqamlar)>=0) {
+    a.forEach(element => {
+        if (!isNaN(element) && Number(element) >= 0) {
             m++;
-        };
+        }
     });
     return m;
-};
+}
 
-const result = task_b("gdghd99u7hn");
-console.log("Result:", result);
-
-/// LESSON
-
-//// CRUD operatsiyalar
-
+console.log(task_b("dfdfhhsdyhsd4h65gdf4615v56z4v8947zsd1g874dzsd1vg")); 
